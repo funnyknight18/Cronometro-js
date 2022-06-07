8650376c659dca59ce3e04676d6fa09c730a4d16
Joao Vitor Mocambite Lima <86741407+funnyknight18@users.noreply.github.com>
2022-06-07 17:01:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -10,29 +10,28 @@ console.log($stop, 'button de stop');
 var $reset = document.querySelector('[data-button="Reset"]');  
 console.log($reset , 'buttao de reset');    
 
-var $time = document.querySelector('data-button="timer"'); 
-console.log($time); 
+var $timer = document.querySelector('[data-button="timer"]'); 
+console.log($timer); 
 
 var interval ;
 
+$start.addEventListener( 'click' , startTimer , false);  //  START 
+$stop.addEventListener( 'click' , stopTimer , false);   // PARAR 
+$reset.addEventListener( 'click', resetTime ,  false);  // RESETAR 
 
 
-// $start.addEventListener('click', function() {
-//    $time.value = +$time.value + 1 ;
-//    setTimeout(startTimer, 1000); 
-// } , false);
-
-
-$start.addEventListener('click' , startTimer, false); 
- function startTimer(){ 
-   $time.value = +$time.value + 1 ; 
-   setTimeout(startTimer , 1000); 
+function startTimer(){ 
+    $timer.value = +$timer.value + 1 ; 
+   interval =  setTimeout(startTimer , 1000); 
 } 
 
+function stopTimer () {
+   clearTimeout(interval); 
+}
 
-$reset.addEventListener('click' , function() {} , false ); 
+function resetTime() {
+  $timer.value = 0; 
+  stopTimer();  
+}
 
-$stop.addEventListener('click' , function() {} , false ); 
-
-
-})(window , document)
+})(window , document);
